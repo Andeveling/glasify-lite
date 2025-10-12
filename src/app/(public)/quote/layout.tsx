35480d16452ee261b86e4e14a92cde66448639a2
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2025-10-12 08:29:28 -0500

    Remove quote components and related layout files for refactoring

--- src/app/(public)/quote/layout.tsx
+++
@@ -1,7 +0,0 @@
-// Force dynamic rendering for quote page
-export const dynamic = 'force-dynamic';
-export const runtime = 'nodejs';
-
-export default function QuoteLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
