c6c0195120dbcdf8a7a7dbb183d7bebc39c51b23
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2025-11-04 08:04:01 -0500

    refactor(playwright): remove commented-out mobile and branded browser configurations
    Cleaned up the Playwright configuration file by removing obsolete comments related to mobile and branded browser testing. This helps streamline the configuration and improve readability.

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,26 +25,6 @@ export default defineConfig({
 			name: "webkit",
 			use: { ...devices["Desktop Safari"] },
 		},
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
 	],
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
