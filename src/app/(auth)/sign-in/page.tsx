eb00311e03eb9a575863783ecdf9611bb357e44a
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2026-04-07 18:01:04 -0500

    fix(auth): add missing /sign-in page route

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SignInForm from "@/app/(auth)/_components/signin-form";
+import AuthCard from "@/app/(auth)/_components/auth-card";
+
+export default function SignInPage() {
+  return (
+    <AuthCard description="Ingresa tus credenciales para continuar" title="Iniciar Sesión">
+      <SignInForm showGoogle={true} />
+    </AuthCard>
+  );
+}
